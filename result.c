e80f60c95dddc9e6878de5252c9e9f835ab85a56
DESKTOP-DFUOKS5\R-417-PC-78 <rabd5050@gmail.com>
2018-08-14 12:41:09 +0600

    Initial Setup 2

---
+++ result.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float c_marks[4];
+int i;
+int j;
+for(j=1;j<=50;j++)
+{printf("student %d",j);
+for (i=1;i<=4;i++)
+
+    {printf("\nc_marks[%d]=",i);
+    scanf("%f",&c_marks);
+}
+}return 0;
+}
